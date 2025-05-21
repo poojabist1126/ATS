e8cd7f82bf1532c5e6b851f139e3e1c26156a0ab
sachinstha9 <sachinshrestha7055@gmail.com>
2025-05-22 10:17:08 +1200

    read and write of csv added

--- ATS/ATS.cpp
+++ ATS/ATS.cpp
@@ -2,8 +2,11 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <fstream>
 #include "productadd_utils.hpp"
 #include "employeeadd_utils.hpp"
+#include "readcsv.hpp"
+#include "writecsv.hpp"
 
 using namespace std;
 
@@ -23,9 +26,22 @@ vector<string> getCommand() {
 }
 
 int main() {
-    vector<string> command;
-    command = getCommand();
-    
+    vector<vector<string>> myData = {
+       {"Bob", "25", "London"},
+       {"Charlie", "35", "This is new"}
+    };
+
+    writeCsvFile("pw.csv", myData, true);
+
+    vector<vector<string>> readData = readCsvFile("pw.csv");
+
+    cout << "\n--- Read Data (with using namespace std;) ---\n";
+    for (const auto& row : readData) {
+        for (const auto& field : row) {
+            cout << field << "\t";
+        }
+        cout << "\n";
+    }
 
     return 0;
 }
