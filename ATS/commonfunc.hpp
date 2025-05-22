ab582135362499dc6db8199e573527fc0e2c8a47
sachinstha9 <sachinshrestha7055@gmail.com>
2025-05-23 11:23:20 +1200

    show feature added

--- ATS/commonfunc.hpp
+++ ATS/commonfunc.hpp
@@ -7,5 +7,6 @@
 std::string getCurrentDateTime();
 std::vector<std::string> getCommand();
 std::string trim(const std::string& s);
+void displayTable(const std::vector<std::vector<std::string>>& data);
 
 #endif
