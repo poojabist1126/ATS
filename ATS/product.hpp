8e33cb027dd658a42bc231eef6677c010747e939
poojabist1126 <270585237@yoobeestudent.ac.nz>
2025-06-05 11:28:17 +1200

    common func cpp updates with some new functions

---
+++ ATS/product.hpp
@@ -0,0 +1,25 @@
+#ifndef PRODUCT_HPP
+#define PRODUCT_HPP
+
+#include <string>
+#include <vector>
+
+class Product {
+private:
+    std::string productId;
+    std::string name;
+    std::string category;
+    std::string price;
+    std::string quantity;
+    std::string res;
+    std::string filePath = "product_details.csv";
+public:
+    std::vector<std::string> askDetails();
+    std::vector<std::string> appendDetails();
+    bool isProductExist(std::string productId);
+    std::vector<std::string> getDetails(std::string productId);
+    bool updateProductDetails(std::string productId);
+    std::vector<std::vector<std::string>> getProductsByfilter(std::string filter);
+};
+
+#endif
