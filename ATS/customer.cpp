fae4162e1c2c8bd158a745101f84df2a6106278f
poojabist1126 <270585237@yoobeestudent.ac.nz>
2025-06-16 11:04:29 +1200

    comments added in customer file

--- ATS/customer.cpp
+++ ATS/customer.cpp
@@ -1,45 +1,21 @@
-#include "customer.hpp"
-#include "commonfunc.hpp"
+#include "admin_.hpp"
 #include "modcsv.hpp"
+#include "commonfunc.hpp"
 #include <iostream>
 
 using namespace std;
 
-vector<string> Customer::askDetails() {
-    while (name.empty() && res != "q") {
+vector<string> Admin::askDetails() {
+    // the while loop loops until a certian function is not met
+    // if typed q whole function gets terminated
+
+    while (username.empty() && res != "q") {
         cout << "Name: ";
-        getline(cin, name);
-
-        if (name.empty()) {
-            cout << "Name cannot be empty. Press enter to type again or type 'q' to quit creating account." << endl;
-            getline(cin, res);
-        }
-
-        if (res == "q")
-            return { };
-    }
-
-    while (!isValidEmail(email) && res != "q") {
-        cout << "Email: ";
-        getline(cin, email);
-
-        if (!isValidEmail(email)) {
-            cout << "Email is not valid. Press enter to type again or type 'q' to quit creating account." << endl;
-            getline(cin, res);
-        }
-
-        if (res == "q")
-            return { };
-    }
-
-    cout << "Thanks for the details.\n" << endl;
-
-    while (res != "q" && isUserExist(username)) {
-        cout << "Username (must be unique): ";
         getline(cin, username);
+        username = trim(username);
 
-        if (isUserExist(username)) {
-            cout << "Username not valid. Press enter to type again or type 'q' to quit creating account." << endl;
+        if (username.empty()) {
+            cout << "Username cannot be empty. Press enter to type again or type 'q' to quit creating account." << endl;
             getline(cin, res);
         }
 
@@ -60,26 +36,29 @@ vector<string> Customer::askDetails() {
             return { };
     }
 
-    return { name, username, email, password, getCurrentDateTime() };
+    return { username, password };
 }
 
-vector<string> Customer::appendDetails() {
+vector<string> Admin::appendDetails() {
     vector<string> details = askDetails();
 
-    writeCsvFile(filePath, { details }, true);
+    writeCsvFile(filePath, { details }, true); // appends details in admin csv file
 
     return details;
 }
 
-bool Customer::isUserExist(string username) {
-    if (username.empty() || username == "")
-        return true;
 
-    vector<vector<string>> details = readCsvFile(filePath);
+bool Admin::isUserExist(string username) {
+    if (username.empty() || username == "")
+        // checks for the value of the parameter
+        return false;
+
+    vector<vector<string>> details = readCsvFile(filePath); // reads admin csv file
     int i = 0;
 
     for (i = 0; i < details.size(); i++) {
-        if (username == details[i][1]) {
+        // details[i][0] means admin username
+        if (username == details[i][0]) {
             return true;
             break;
         }
@@ -88,74 +67,18 @@ bool Customer::isUserExist(string username) {
     return false;
 }
 
-vector<string> Customer::getDetails(string username) {
+vector<string> Admin::getDetails(string username) {
     if (isUserExist(username)) {
         vector<vector<string>> details = readCsvFile(filePath);
         int i = 0;
 
         for (i = 0; i < details.size(); i++) {
-            if (trim(username) == details[i][1]) {
-                return {trim(details[i][0]), trim(details[i][1]), trim(details[i][2]), trim(details[i][3]), trim(details[i][4]) };
+            if (trim(username) == details[i][0]) {
+                return { trim(details[i][0]), trim(details[i][1]) }; // returns username and password if the user exists
                 break;
             }
         }
     }
 
     return {};
-}
-
-bool Customer::updateUserDetails(string username) {
-    cout << "Name: ";
-    getline(cin, name);
-
-    while (!isValidEmail(email) && res != "q" ) {
-        cout << "Email: ";
-        getline(cin, email);
-
-        if (email.empty())
-            break;
-
-        if (!isValidEmail(email)) {
-            cout << "Email is not valid. Press enter to type again or type 'q' to quit updating details." << endl;
-            getline(cin, res);
-        }
-
-        if (res == "q")
-            return false;
-    }
-    
-    while (!isValidPassword(password) && res != "q") {
-        cout << "Password (must be atleast 6 characters, must contain atleast one uppercase letter and one number): ";
-        getline(cin, password);
-
-        if (password.empty())
-            break;
-
-        if (!isValidPassword(password)) {
-            cout << "Password is not valid. Press enter to type again or type 'q' to quit updating details." << endl;
-            getline(cin, res);
-        }
-
-        if (res == "q")
-            return { };
-    }
-
-    updateDetails(filePath, 1, trim(username), { trim(name), trim(username), trim(email), trim(password), "" });
-    return true;
-}
-
-void Customer::deleteUser(string username) {
-    if (getDetails(username).empty())
-        cout << "Customer not found." << endl;
-    else
-        deleteDetails(filePath, 1, username);
-}
-
-void Customer::clear() {
-    name.clear();
-    username.clear();
-    email.clear();
-    password.clear();
-    joinedDate.clear();
-    res.clear();
 }
