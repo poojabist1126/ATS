36ba523ff1ad3e77e53942f4ab0be29940ed0da0
sachinstha9 <sachinshrestha7055@gmail.com>
2025-06-17 11:22:39 +1200

    comments added in customer cpp file

--- ATS/admin_.cpp
+++ ATS/admin_.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 vector<string> Admin::askDetails() {
+    // the while loop loops until a certian function is not met
+    // if typed q whole function gets terminated
+
     while (username.empty() && res != "q") {
         cout << "Name: ";
         getline(cin, username);
@@ -39,7 +42,7 @@ vector<string> Admin::askDetails() {
 vector<string> Admin::appendDetails() {
     vector<string> details = askDetails();
 
-    writeCsvFile(filePath, { details }, true);
+    writeCsvFile(filePath, { details }, true); // appends details in admin csv file
 
     return details;
 }
@@ -47,12 +50,14 @@ vector<string> Admin::appendDetails() {
 
 bool Admin::isUserExist(string username) {
     if (username.empty() || username == "")
+        // checks for the value of the parameter
         return false;
 
-    vector<vector<string>> details = readCsvFile(filePath);
+    vector<vector<string>> details = readCsvFile(filePath); // reads admin csv file
     int i = 0;
 
     for (i = 0; i < details.size(); i++) {
+        // details[i][0] means admin username
         if (username == details[i][0]) {
             return true;
             break;
@@ -69,7 +74,7 @@ vector<string> Admin::getDetails(string username) {
 
         for (i = 0; i < details.size(); i++) {
             if (trim(username) == details[i][0]) {
-                return { trim(details[i][0]), trim(details[i][1]) };
+                return { trim(details[i][0]), trim(details[i][1]) }; // returns username and password if the user exists
                 break;
             }
         }
--- ATS/customer.cpp
+++ ATS/customer.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 vector<string> Customer::askDetails() {
+    // the while loop loops until a certain function is not met
+    // if typed q whole function gets terminated
+
     while (name.empty() && res != "q") {
         cout << "Name: ";
         getline(cin, name);
@@ -66,20 +69,22 @@ vector<string> Customer::askDetails() {
 vector<string> Customer::appendDetails() {
     vector<string> details = askDetails();
 
-    writeCsvFile(filePath, { details }, true);
+    writeCsvFile(filePath, { details }, true); // appends details to customer_details.csv file
 
     return details;
 }
 
 bool Customer::isUserExist(string username) {
     if (username.empty() || username == "")
+        // checks if the username is empty or not
         return true;
 
-    vector<vector<string>> details = readCsvFile(filePath);
+    vector<vector<string>> details = readCsvFile(filePath); // read customer.csv file
     int i = 0;
 
     for (i = 0; i < details.size(); i++) {
         if (username == details[i][1]) {
+            // checks for the specific user, if found returns true
             return true;
             break;
         }
@@ -90,21 +95,26 @@ bool Customer::isUserExist(string username) {
 
 vector<string> Customer::getDetails(string username) {
     if (isUserExist(username)) {
-        vector<vector<string>> details = readCsvFile(filePath);
+        vector<vector<string>> details = readCsvFile(filePath); // reads customer.csv file
         int i = 0;
 
         for (i = 0; i < details.size(); i++) {
             if (trim(username) == details[i][1]) {
+                // if the username is matched returns customer details
                 return {trim(details[i][0]), trim(details[i][1]), trim(details[i][2]), trim(details[i][3]), trim(details[i][4]) };
                 break;
             }
         }
     }
 
-    return {};
+    return {}; // returns empty 
 }
 
 bool Customer::updateUserDetails(string username) {
+    // the while loop loops until a certain function is not met
+    // if typed q whole function gets terminated
+    // if left blanks the specific one is skipped
+
     cout << "Name: ";
     getline(cin, name);
 
@@ -113,7 +123,7 @@ bool Customer::updateUserDetails(string username) {
         getline(cin, email);
 
         if (email.empty())
-            break;
+            break; 
 
         if (!isValidEmail(email)) {
             cout << "Email is not valid. Press enter to type again or type 'q' to quit updating details." << endl;
@@ -140,7 +150,7 @@ bool Customer::updateUserDetails(string username) {
             return { };
     }
 
-    updateDetails(filePath, 1, trim(username), { trim(name), trim(username), trim(email), trim(password), "" });
+    updateDetails(filePath, 1, trim(username), { trim(name), trim(username), trim(email), trim(password), "" }); // if parameter is empty the value doesnot change
     return true;
 }
 
@@ -148,10 +158,11 @@ void Customer::deleteUser(string username) {
     if (getDetails(username).empty())
         cout << "Customer not found." << endl;
     else
-        deleteDetails(filePath, 1, username);
+        deleteDetails(filePath, 1, username); // removes the user details from the csv file
 }
 
 void Customer::clear() {
+    // clears every variables
     name.clear();
     username.clear();
     email.clear();
